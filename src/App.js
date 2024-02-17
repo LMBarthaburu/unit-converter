ff2a12e763ce5c295897003b5ef66faf3abebde3
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2024-02-17 18:40:45 +0100

    conversor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Conversor from './Components/Navbar/Conversor/Conversor'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Conversor/>
     </>
   )
 }
