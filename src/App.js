da162974d39f0a8963e4ea96f1c7943be1b7d536
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2024-02-19 00:02:12 +0100

    footer y cambios esteticos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Conversor from './Components/Conversor/Conversor'
 import Saved from './Components/Saved/Saved'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Conversor/>
-      <Saved/>
+      <div className='contenedor'>
+        <Conversor/>
+        <Saved/>
+      </div>
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Terms of service</p>
+      <p>Privacy policy</p>
+    </div>
+  )
+}
+
+export default Footer
